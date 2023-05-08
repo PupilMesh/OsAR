66cf87de5114b0f1a54904eec47fa4aa086453e5
Krishnan R <vinokrish001@gmail.com>
2023-05-08 19:36:08 +0530

    updated readme

---
+++ packages/mobile/src/components/BabylonFunctions/IMU.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { accelerometer, gyroscope, setUpdateIntervalForType, SensorTypes } from "react-native-sensors";
