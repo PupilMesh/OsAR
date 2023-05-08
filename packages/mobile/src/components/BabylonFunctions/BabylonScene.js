a0eb0b30efd470047825b0ff10f0e2c517fa3a67
Krishnan R <vinokrish001@gmail.com>
2023-05-08 12:56:29 +0530

    loader function added

--- packages/mobile/src/components/BabylonFunctions/BabylonScene.js
+++ packages/mobile/src/components/BabylonFunctions/BabylonScene.js
@@ -5,38 +5,38 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 
-import React, { useCallback } from 'react';
+import React, { useCallback,useState,useEffect } from 'react';
 import { Behavior, Color3, Color4, Engine, HemisphericLight, Mesh, MeshBuilder, Node, Nullable, Observer, Scene, StandardMaterial, UniversalCamera, Vector3 } from '@babylonjs/core';
 import GLRenderer from '../GLRenderer';
 import { SceneLoader } from "@babylonjs/core/Loading/sceneLoader";
-export default function BabylonScene() {
 
-  const onCreateEngine = useCallback((engine) => {
+export default function BabylonScene() {
+  const [engine, setEngine] = useState(null);
+
+  const loadModel = (url, position) => {
     if (!engine) return;
+
+    SceneLoader.ImportMesh('', url, '', engine.scenes[0], function (newMeshes) {
+      const root = newMeshes[0];
+      root.position.set(position.x, position.y, position.z);
+      root.scaling = new Vector3(0.01, 0.01, 0.01); // Adjust the scaling if needed
+    });
+  };
+
+  const onCreateEngine = (engine) => {
+    if (!engine) return;
+
+    setEngine(engine);
 
     const scene = new Scene(engine);
 
-    scene.clearColor = Color4.FromHexString(`#000000`);
+    scene.clearColor = Color4.FromHexString('#000000');
 
-    const camera = new UniversalCamera('camera', new Vector3(0, 3, -5), scene);
+    const camera = new UniversalCamera('camera', new Vector3(0, 1.6, -5), scene);
     camera.setTarget(Vector3.Zero());
+    camera.attachControl(engine.getRenderingCanvas(), true);
 
     const light = new HemisphericLight('HemiLight', new Vector3(0, 9, -5), scene);
-
-    // const box = MeshBuilder.CreateBox('Cube', { size: 1 });
-    // const boxMaterial = new StandardMaterial('CubeMaterial', scene);
-    // boxMaterial.diffuseColor = Color3.FromHexString('#0081fe');
-    // box.material = boxMaterial;
-
-    // box.position.set(0, 0, 0);
-    // box.addRotation(0.1, 0.5, 0);
-
-  const modelUrl = "https://res.cloudinary.com/doblnhena/image/upload/v1683485043/2CylinderEngine_lfapqb.glb"; // Replace with your glb file path
-  SceneLoader.ImportMesh("", modelUrl, "", scene, function (newMeshes) {
-    const root = newMeshes[0];
-    root.position.set(0, 0, 0);
-    root.scaling = new Vector3(0.01, 0.01, 0.01); // Adjust the scaling if needed
-  });
 
     engine.runRenderLoop(function () {
       if (scene && scene.activeCamera) scene.render();
@@ -47,11 +47,31 @@ export default function BabylonScene() {
       camera.dispose();
       engine.dispose();
     };
-  }, []);
+  };
+
+  // Load initial model
+  useEffect(() => {
+    if (engine) {
+      const modelUrl = 'https://res.cloudinary.com/doblnhena/image/upload/v1683485043/2CylinderEngine_lfapqb.glb';
+      loadModel(modelUrl, new Vector3(0, 0, 0));
+    }
+  }, [engine]);
+
+  // Example of how to add a new model to the scene dynamically after 5 seconds
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (engine) {
+        const modelUrl = 'https://res.cloudinary.com/doblnhena/image/upload/v1683485043/2CylinderEngine_lfapqb.glb';
+        loadModel(modelUrl, new Vector3(1, 0, 0));
+      }
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [engine]);
 
   return (
     <>
       <GLRenderer onCreateEngine={onCreateEngine} />
     </>
-)
+  );
 }
