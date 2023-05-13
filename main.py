c0edc7a1f48d4455ce7f05777c550dc7417e8420
Serpent03 <dumbpilot03@gmail.com>
2023-05-13 18:50:15 +0530

    feat: triangulation & point cloud

--- main.py
+++ main.py
@@ -1,40 +1,93 @@
+import numpy as np
 import cv2
 import os
 
 from utils import *
 
+# Fisheye camera and distortion matrices
+K=np.array([[455.5000196386718, 0.0, 482.65324003911945], 
+            [0.0, 340.6409393462825, 254.5063795692748], 
+            [0.0, 0.0, 1.0]])
+D=np.array([[-0.018682808343432777], 
+            [-0.044315351694893736], 
+            [0.047678551616171246], 
+            [-0.018283908577445218]])
+
+K = retCamMtx("./data/calibration/cameraMatrix.pkl")
+D = retDistCoeff("./data/calibration/dist.pkl")
+
+orb = cv2.ORB_create(nfeatures=1000)
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
+
 imDIR = './data/images/'
-limiter = 1200 # number of matches. cv2 errors out if I utilize more than 1200. Why ??
+limiter = 100 # number of matches
 imgs = os.listdir(imDIR)
 imSEQ = list(zip(imgs, imgs[1:]))
-pts_3d = []
-
-camMtx = retCamMtx("./data/calibration/cameraMatrix.pkl")
-nCamMtx = retCamMtx("./data/calibration/newCameraMatrix.pkl")
-distCoeff = retDistCoeff("./data/calibration/dist.pkl")
+pts_3d = np.array([0,0,0])
 
 for i in imSEQ:
-    img1 = readIm(f'{imDIR}{i[0]}')
-    img2 = readIm(f'{imDIR}{i[1]}')
-    img1 = retUndistortedIm(img1, camMtx, nCamMtx, distCoeff)
-    img2 = retUndistortedIm(img2, camMtx, nCamMtx, distCoeff)
+    img0 = cv2.imread(f"{imDIR}{i[0]}")
+    img2 = cv2.imread(f"{imDIR}{i[1]}")
 
-    kp1, des1, kp2, des2 = ORB_detector(img1, img2, limiter)
-    # kp1, des1, kp2, des2 = BRISK_detector(img1, img2)
-    kpL1 = retKpList(kp1)
-    kpL2 = retKpList(kp2)
-    numMatches = bruteForceMatcher(des1, des2)
+    # Find keypoints and match them up
+    kp0, des0 = orb.detectAndCompute(img0, None)
+    kp2, des2 = orb.detectAndCompute(img2, None)
+    matches = bf.knnMatch(des0, des2, k=2)
 
-    essMtx, _ = retEssentialMat(kpL1, kpL2, camMtx, distCoeff)
-    _, R, t, mask = retPoseRecovery(essMtx, kpL1, kpL2)
-    pts_3d.extend(retTriangulation(R, t, kpL1, kpL2, limiter))
-    # print(f'{pts_3d}')
-    display2D(img1, kp1, img2, kp2, numMatches)
-    cv2.waitKey(1)
+    display2DKNN(img0, kp0, img2, kp2, matches[-20:])
 
-# FIXME: almost no depth in the images, might be caused
-# FIXME: by incorrect calibration matrix.
-display3D(pts_3d)
+    # Find good matches using the ratio test
+    ratio_thresh = 0.9
+    good_matches = []
+    for m,n in matches:
+        if m.distance < ratio_thresh * n.distance:
+            good_matches.append(m)
 
-cv2.destroyAllWindows()
+    # Convert from keypoints to points
+    pts0 = np.float32([kp0[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+    pts2 = np.float32([kp2[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
 
+    # pts0, pts2 = retGoodPoints(kp0, kp2, matches)
+
+    # Remove the fisheye distortion from the points
+    # pts0 = cv2.fisheye.undistortPoints(pts0, K, D, P=K)
+    # pts2 = cv2.fisheye.undistortPoints(pts2, K, D, P=K)
+    pts0 = cv2.undistortPoints(pts0, K, D, P=K)
+    pts2 = cv2.undistortPoints(pts2, K, D, P=K)
+
+    # Keep only the points that make geometric sense
+    # TODO: find a more efficient way to apply the mask
+    E, mask = cv2.findEssentialMat(pts0, pts2, K, cv2.RANSAC, 0.999, 1, None)
+    _, R, t, mask = cv2.recoverPose(E, pts0, pts2, cameraMatrix=K, mask=mask)
+    pts0_m = []
+    pts2_m = []
+    for i in range(len(mask)):
+        print(mask[i])
+        if mask[i] == 1:
+            pts0_m.append(pts0[i])
+            pts2_m.append(pts2[i])
+    pts0 = np.array(pts0_m).T.reshape(2, -1)
+    pts2 = np.array(pts2_m).T.reshape(2, -1)
+
+    # Setup the projection matrices
+    R = np.eye(3)
+    t0 = np.array([[0], [0], [0]])
+    t2 = t
+    # t2 = np.array([[0], [0], [2]])
+    P0 = np.dot(K, np.concatenate((R, t0), axis=1))
+    P2 = np.dot(K, np.concatenate((R, t2), axis=1))
+
+    # Find the keypoint world homogeneous coordinates assuming img0 is the world origin
+    X = cv2.triangulatePoints(P0, P2, pts0, pts2)
+
+    # Convert from homogeneous cooridinates
+    X /= X[3]
+    objPts = X.T[:,:3]
+    np.append(pts_3d, objPts)
+
+    # Find the pose of the second frame
+    _, rvec, tvec, inliers = cv2.solvePnPRansac(objPts, pts2.T, K, None)
+# print(rvec)
+    print(tvec)
+    cv2.waitKey(20)
+display3D(objPts)
--- utils.py
+++ utils.py
@@ -4,11 +4,11 @@ import numpy as np
 import plotly.graph_objects as go
 
 def readIm(pathToIm):
-    rFac = 6
+    rFac = 1
     im = cv2.imread(pathToIm)
     im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     im = np.array(im)
-    im = cv2.GaussianBlur(im, (5,5), 0)
+    im = cv2.GaussianBlur(im, (3,3), 0)
     im =  cv2.resize(im, (
         int(im.shape[1] / rFac), # width
         int(im.shape[0] / rFac), # height
@@ -17,8 +17,13 @@ def readIm(pathToIm):
     return im
 
 def retUndistortedIm(im, camMtx, nCamMtx, distCoeff):
-    imUndistorted = cv2.undistort(im, camMtx, None, None, nCamMtx)
+    imUndistorted = cv2.undistort(im, camMtx, distCoeff, None, nCamMtx)
     return imUndistorted
+
+def retUndistortedPts(kp1, kp2, camMtx, distCoeff):
+    pts1 = cv2.undistortPoints(kp1, camMtx, distCoeff, P=camMtx)
+    pts2 = cv2.undistortPoints(kp2, camMtx, distCoeff, P=camMtx)
+    return pts1, pts2
 
 def retCamMtx(pathToCamMtx):
     with open(f'{pathToCamMtx}', 'rb') as f:
@@ -35,12 +40,6 @@ def ORB_detector(im1, im2, limiter):
     # print(kp1[0].pt)
     return (kp1, des1, kp2, des2)
 
-def BRISK_detector(im1, im2):
-    brisk = cv2.BRISK_create(thresh=100)
-    kp1, des1 = brisk.detectAndCompute(im1, None)
-    kp2, des2 = brisk.detectAndCompute(im2, None)
-    return (kp1, des1, kp2, des2)
-
 def retKpList(kp):
     kpL = []
     for i in kp:
@@ -48,48 +47,81 @@ def retKpList(kp):
     return np.array(kpL)
 
 def bruteForceMatcher(des1, des2):
-    bfm = cv2.BFMatcher_create(cv2.NORM_HAMMING2, crossCheck=True)
+    bfm = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
     numMatches = bfm.match(des1, des2)
     numMatches = sorted(numMatches,key=lambda x:x.distance)
     # print(numMatches[0].imgIdx)
     return numMatches
 
 def bruteForceMatcherkNN(des1, des2):
-    bfm = cv2.BFMatcher_create(cv2.NORM_HAMMING2, crossCheck=True)
-    matches = bfm.knnMatch(des1, des2, k=1)
+    bfm = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
+    matches = bfm.knnMatch(des1, des2, k=2)
     return matches
 
-def retGoodPoints(kps, bfMatches):
-    points = np.float32([kps[m.queryIdx].pt for m in bfMatches]).reshape(-1, 1, 2)
+def retGoodPoints(kp1, kp2, bfMatches):
+    good_matches = []
+    ratio = 0.8
+    for m, n in bfMatches:
+        if m.distance < ratio * n.distance:
+            good_matches.append(m)
+    pts1 = np.float32([kp1[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+    pts2 = np.float32([kp2[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+    return pts1, pts2
 
-def retEssentialMat(kpL1, kpL2, camMtx, dist):
+def retEssentialMat(kpL1, kpL2, camMtx, distCoeff):
     # print(type(kpL1))
-    # kpL1 = np.array(kpL1) # convert to np.array just before operation
-    # kpL2 = np.array(kpL2)
-    # dist = np.array(dist)
+    kpL1 = np.array(kpL1) # convert to np.array just before operation
+    kpL2 = np.array(kpL2)
     # NOTE : we convert them to np.array because cv2 expects a 
     # NOTE <cv::UMat> format 
-    c =  cv2.findEssentialMat(kpL1, kpL2, camMtx, None, None, None, None)
+    c =  cv2.findEssentialMat(kpL1, kpL2, camMtx)
     return c
 
-def retPoseRecovery(essMtx, kpL1, kpL2):
-    c = cv2.recoverPose(essMtx, kpL1, kpL2)
+def retPoseRecovery(essMtx, kpL1, kpL2, camMtx, mask):
+    print(essMtx)
+    c = cv2.recoverPose(essMtx, kpL1, kpL2, camMtx, mask=mask)
     return c
 
-def retTriangulation(_R, _t, kpL1, kpL2, limiter):
+def retGoodGeometricPoints(mask, kp1, kp2):
+    pts1 = []
+    pts2 = []
+    for i in range(len(mask)):
+        print(mask[i])
+        if mask[i] == 1: # this returns 1 only after undistortion
+            pts1.append(kp1[i])
+            pts2.append(kp2[i])
+    pts1_good = np.array(pts1).T.reshape(2, -1)
+    pts2_good = np.array(pts2).T.reshape(2, -1)
+    return pts1_good, pts2_good
+
+def retTriangulation(_R, _t, kp1, kp2, camMtx):
     pts_3d = []
-    proj_matrix = np.hstack((_R, _t))
-    # print(kpL2[0])
-    for i in range(len(kpL1)):
-        pts_4d = cv2.triangulatePoints(np.eye(3, 4), proj_matrix, kpL1[i], kpL2[i])
-        pts = cv2.convertPointsFromHomogeneous(pts_4d.T)
-        # print(pts)
-        pts_3d.append([pts[0][0][0], pts[0][0][1], pts[0][0][2]])
-    return pts_3d
+    # Set up the scene such that the first image
+    # is the world origin
+    R = np.eye(3)
+    t1 = np.array([[0], [0], [0]])
+    t2 = _t
+    P1 = np.dot(camMtx, np.concatenate((R, t1), axis=1))
+    P2 = np.dot(camMtx, np.concatenate((R, t2), axis=1))
+
+    X = cv2.triangulatePoints(P1, P2, kp1, kp2)
+    X /= X[3]
+    objPts = X.T[:,:3]
+
+    # pts4d = cv2.triangulatePoints(proj_matrix0, proj_matrix, kpL1.T, kpL2.T).T
+    # pts4d /= pts4d[:, 3:]
+    # out = list(np.delete(pts4d, 3, 1))
+    # pts_3d.append(out)
+
+    return objPts
     # return -1
 
 def display2D(img1, kp1, img2, kp2, numMatches):
     out = cv2.drawMatches(img1, kp1, img2, kp2, numMatches, None)
+    cv2.imshow('Image', out)
+
+def display2DKNN(img1, kp1, img2, kp2, numMatches):
+    out = cv2.drawMatchesKnn(img1, kp1, img2, kp2, numMatches, None)
     cv2.imshow('Image', out)
 
 def display3D(pointCloud):
@@ -97,12 +129,12 @@ def display3D(pointCloud):
     y = []
     z = []
 
-    for i in pointCloud:
-        x.append(i[0])
-        y.append(i[1])
-        z.append(i[2])
+    print(pointCloud)
 
-    print(z)
+    for imgPts in pointCloud:
+        x.append(imgPts[0])
+        y.append(imgPts[1])
+        z.append(imgPts[2])
 
     markerData = go.Scatter3d(
         x = x,
@@ -116,5 +148,70 @@ def display3D(pointCloud):
     fig = go.Figure(data=markerData)
     fig.show()
 
-    # print(x)
     # print(pointCloud)
+
+def linear_LS_triangulation(u1, P1, u2, P2):
+    linear_LS_triangulation_C = -np.eye(2, 3)
+    """
+    Linear Least Squares based triangulation.
+    Relative speed: 0.1
+    
+    (u1, P1) is the reference pair containing normalized image coordinates (x, y) and the corresponding camera matrix.
+    (u2, P2) is the second pair.
+    
+    u1 and u2 are matrices: amount of points equals #rows and should be equal for u1 and u2.
+    
+    The status-vector will be True for all points.
+    """
+    A = np.zeros((4, 3))
+    b = np.zeros((4, 1))
+    
+    # Create array of triangulated points
+    x = np.zeros((3, len(u1)))
+    
+    # Initialize C matrices
+    C1 = np.array(linear_LS_triangulation_C)
+    C2 = np.array(linear_LS_triangulation_C)
+    
+    for i in range(len(u1)):
+        # Derivation of matrices A and b:
+        # for each camera following equations hold in case of perfect point matches:
+        #     u.x * (P[2,:] * x)     =     P[0,:] * x
+        #     u.y * (P[2,:] * x)     =     P[1,:] * x
+        # and imposing the constraint:
+        #     x = [x.x, x.y, x.z, 1]^T
+        # yields:
+        #     (u.x * P[2, 0:3] - P[0, 0:3]) * [x.x, x.y, x.z]^T     +     (u.x * P[2, 3] - P[0, 3]) * 1     =     0
+        #     (u.y * P[2, 0:3] - P[1, 0:3]) * [x.x, x.y, x.z]^T     +     (u.y * P[2, 3] - P[1, 3]) * 1     =     0
+        # and since we have to do this for 2 cameras, and since we imposed the constraint,
+        # we have to solve 4 equations in 3 unknowns (in LS sense).
+
+        # Build C matrices, to construct A and b in a concise way
+        C1[:, 2] = u1[i, :]
+        C2[:, 2] = u2[i, :]
+        
+        # Build A matrix:
+        # [
+        #     [ u1.x * P1[2,0] - P1[0,0],    u1.x * P1[2,1] - P1[0,1],    u1.x * P1[2,2] - P1[0,2] ],
+        #     [ u1.y * P1[2,0] - P1[1,0],    u1.y * P1[2,1] - P1[1,1],    u1.y * P1[2,2] - P1[1,2] ],
+        #     [ u2.x * P2[2,0] - P2[0,0],    u2.x * P2[2,1] - P2[0,1],    u2.x * P2[2,2] - P2[0,2] ],
+        #     [ u2.y * P2[2,0] - P2[1,0],    u2.y * P2[2,1] - P2[1,1],    u2.y * P2[2,2] - P2[1,2] ]
+        # ]
+        A[0:2, :] = C1.dot(P1[0:3, 0:3])    # C1 * R1
+        A[2:4, :] = C2.dot(P2[0:3, 0:3])    # C2 * R2
+        
+        # Build b vector:
+        # [
+        #     [ -(u1.x * P1[2,3] - P1[0,3]) ],
+        #     [ -(u1.y * P1[2,3] - P1[1,3]) ],
+        #     [ -(u2.x * P2[2,3] - P2[0,3]) ],
+        #     [ -(u2.y * P2[2,3] - P2[1,3]) ]
+        # ]
+        b[0:2, :] = C1.dot(P1[0:3, 3:4])    # C1 * t1
+        b[2:4, :] = C2.dot(P2[0:3, 3:4])    # C2 * t2
+        b *= -1
+        
+        # Solve for x vector
+        cv2.solve(A, b, x[:, i:i+1], cv2.DECOMP_SVD)
+    
+    return x.T.astype(float), np.ones(len(u1), dtype=bool)
